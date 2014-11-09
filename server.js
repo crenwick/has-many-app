f3f4ed9e3adc934e879069d04cbc351945d0f6db
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-09 14:09:48 -0800

    dependances and package made

---
+++ server.js
@@ -0,0 +1,18 @@
+/*jshint node:true*/
+'use strict';
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var app = express();
+
+app.use(bodyparser.json());
+
+mongoose.connect(process.env.MONGO_URL || process.env.MONGOLAD_URI || 'mongodb://localhost/has_many_development');
+
+//require('./routes/
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+    console.log('server running on port: %d', app.get('port'));
+});
